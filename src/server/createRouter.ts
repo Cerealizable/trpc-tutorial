a265ce839c1d2906a54b185b4d88e0147527cc48
Cerealizable <alberstc1@gmail.com>
2022-07-13 15:33:18 -0700

    feat: posts

--- src/server/createRouter.ts
+++ src/server/createRouter.ts
@@ -3,6 +3,5 @@ import superjson from 'superjson';
 import { Context } from './createContext';
 
 export function createRouter() {
-	// todo add context to generic
 	return router<Context>().transformer(superjson);
 }
